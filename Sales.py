23adbc61fcc58e58bde5cbf6f38f81bab50d7764
KiomeGM <rogerskiome.rk@gmail.com>
2024-01-02 14:35:36 +0300

    Added sales.py

---
+++ Sales.py
@@ -0,0 +1,29 @@
+"""_summary_
+This is a sales module for PyE POS System.
+
+It contains functions that will:
+    1. Check if a product is in inventory
+    2. Check if quantity in inventory is atleast equal to quantity sold
+    3. Calculate total amount client should pay
+    4. Record completion of sale
+    5. Remove quantity of product(s) in inventory after sale is complete
+    6. Allow for return of products(s) if products are non-food items.
+        - The request is sent to supervisor
+        - Supervisor approves/disapproves return and decision sent to Counter System
+        - If approved, product(s) quantity in inventory is increased, and price deducted from the sales amount 
+"""
+
+def checkProductAvailability():
+    pass
+
+def productSales():
+    pass
+
+def productReturns():
+    pass
+
+def productReturnsApproval():
+    pass
+
+def inventoryChange():
+    pass
