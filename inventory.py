2eb88575b856b13ef68b567d84271671bfd450aa
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-02-18 19:20:00 +0300

    feat: pos function based template 	1. Declare empty functions 	2. Define data storage strategy for later implementation by team

---
+++ inventory.py
@@ -0,0 +1,87 @@
+"""
+This is the implementation of Python Expert Computer shop.
+Authors:
+1. Caroline
+2. Kelvin
+3. Rogers
+
+Key Functions
+1. List categories and products available.
+2. Delete or add product categories or products.
+   Question: What happens if you delete a product category?
+3. Product selling
+4. Sale reports
+5.
+6.
+"""
+
+# imports
+
+# ********** The shop stocks various categories of product
+# Data storage strategy
+# 1. Store all available categories of products(id, category_name)
+# 2. Store all available products(id, product_name, description,category, unit_price)
+# 3. Store stock levels for each product(id, product_name, stock_levels)
+# 4. Record sales(id, quantity_sold, unit_price, total)
+
+
+def list_products():
+    """
+    List all available products
+    :return:
+    """
+    pass
+
+
+def add_product():
+    """
+    Add a product to the inventory
+    """
+    pass
+
+
+def delete_product():
+    """
+    Delete a product from the inventory
+    :return:
+    """
+    pass
+
+
+def list_categories():
+    """
+    List all available product categories
+    :return:
+    """
+    pass
+
+
+def add_category():
+    """
+    Add a category to the inventory
+    :return:
+    """
+    pass
+
+
+def delete_category():
+    """
+    Delete a category from the inventory
+    """
+    pass
+
+
+def restock():
+    """
+    Restock a product from the inventory
+    :return:
+    """
+    pass
+
+
+def sale():
+    """
+    Sale a product from the inventory
+    :return
+    """
+    pass
