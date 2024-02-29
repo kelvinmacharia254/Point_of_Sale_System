0ad7973dd3c7e44f29826b5193ddba0f01e29d87
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-02-29 17:19:27 +0300

    feat: product category functions
       1. add category add_category() function code.
       2. add category list_categories() function code.
       3. add category delete_category() function code.
       4. improve write_to_file() function logic to use different file writing modes on conditions.

--- inventory.py
+++ inventory.py
@@ -15,6 +15,7 @@ Key Functions
 6.
 """
 
+
 # imports
 
 # ********** The shop stocks various categories of product
@@ -53,7 +54,10 @@ def list_categories():
     List all available product categories
     :return:
     """
-    pass
+    categories = read_file("categories.txt")
+    print("Product Categories:")
+    for i, category in enumerate(categories):
+        print(str(i + 1) + "." + category.strip())
 
 
 def add_category():
@@ -61,6 +65,8 @@ def add_category():
     Add a category to the inventory
     :return:
     """
+    category = "laptops\n"
+    write_to_file(category, "categories.txt", mode="a")
     pass
 
 
@@ -68,7 +74,27 @@ def delete_category():
     """
     Delete a category from the inventory
     """
-    pass
+    category_to_delete = "headsets\n"
+    categories = read_file("categories.txt")
+    print(categories)
+    if category_to_delete in categories:
+        while True:
+            print("You are about to delete the '" + category_to_delete+"' category.")
+            print("Are you sure you want to delete this category?")
+            answer = input("[Y]es to delete | [N]o to cancel deletion.\n ")
+            if answer.upper() == "Y":
+                categories.remove(category_to_delete)
+                print("You have successfully deleted the '" + category_to_delete+"' category.")
+                write_to_file(categories, "categories.txt", mode="w")
+                break
+            elif answer.upper() == "N":
+                print("You have cancelled the deletion.")
+                break
+            else:
+                print("Invalid input entered.")
+
+    else:
+        print("There is no such category.")
 
 
 def restock():
@@ -85,3 +111,56 @@ def sale():
     :return
     """
     pass
+
+
+def read_file(file_name):
+    """Reads in a file and stores each line as an element in a list
+
+    [IMPLEMENT ME]
+        1. Open the given file
+        2. Read the file line by line and append each line to a list
+        3. Return the list
+
+    Args:
+        file_name: the name of the file to be read
+
+    Returns:
+        list: a list where each element is a line in the file.
+    """
+
+    with open(file_name) as f:
+        content_list = f.readlines()
+        return content_list
+
+
+def write_to_file(file_contents, output_filename, mode):
+    """Writes the first line of a string to a file.
+
+    [IMPLEMENT]
+        1. Get the first line of file_contents
+        2. Use the File write() function to write the first line into a file
+           with the name from output_filename
+
+        We determine the first line to be everything in a string before the
+        first newline ('\n') character.
+
+    Args:
+        file_contents: string to be split and written into output file
+        output_filename: the name of the file to be written to
+    """
+
+    content = file_contents
+    f = open(output_filename, mode)
+    if mode == "a":
+        f.write(content)
+    else:  # modes = "w"
+        print(content)
+        f.writelines(content)
+
+
+list_categories()
+
+# add_category()
+delete_category()
+
+list_categories()
