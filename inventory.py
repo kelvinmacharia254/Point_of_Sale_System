4d376fe81f2fec106cf0356f9835fd089678829d
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-03-10 09:33:12 +0300

    fix: product category management 		1. Implement read_file function 		2. Implement write_to_file function 		3. Implement add, list and delete product category functions

--- inventory.py
+++ inventory.py
@@ -13,18 +13,20 @@ Key Functions
 4. Sale reports
 5.
 6.
+
+# *** The shop stocks various categories of product
+Data storage strategy
+# 1. Store all available categories of products(id, category_name)
+# 2. Store all available products (id, product_name, description, category, unit_price)
+# 3. Store stock levels for each product (id, product_name, stock_levels)
+# 4. Record sales (id, quantity_sold, unit_price, total)
+
 """
 
+import logging
 
-# imports
-
-# ********** The shop stocks various categories of product
-# Data storage strategy
-# 1. Store all available categories of products(id, category_name)
-# 2. Store all available products(id, product_name, description,category, unit_price)
-# 3. Store stock levels for each product(id, product_name, stock_levels)
-# 4. Record sales(id, quantity_sold, unit_price, total)
-
+logging.basicConfig(level=logging.CRITICAL, format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt="%d-%b-%y %H:%M:%S")
 
 def list_products():
     """
@@ -56,8 +58,11 @@ def list_categories():
     """
     categories = read_file("categories.txt")
     print("Product Categories:")
-    for i, category in enumerate(categories):
-        print(str(i + 1) + "." + category.strip())
+    if len(categories) > 0:
+        for i, category in enumerate(categories):
+            print(str(i + 1) + "." + category.strip())
+    else:
+        print("No categories found")
 
 
 def add_category():
@@ -65,26 +70,27 @@ def add_category():
     Add a category to the inventory
     :return:
     """
-    category = "laptops\n"
-    write_to_file(category, "categories.txt", mode="a")
-    pass
+    categories = "Flash drives\n"
+    # categories = ['RAM\n', 'Storage\n', 'Monitors\n']
+    write_to_file(categories, "categories.txt", mode="a")
 
 
 def delete_category():
     """
-    Delete a category from the inventory
+    Delete a category from the inventory.
+
     """
-    category_to_delete = "headsets\n"
+    category_to_delete = "RAM\n"
     categories = read_file("categories.txt")
     print(categories)
     if category_to_delete in categories:
         while True:
-            print("You are about to delete the '" + category_to_delete+"' category.")
+            print("You are about to delete the '" + category_to_delete + "' category.")
             print("Are you sure you want to delete this category?")
             answer = input("[Y]es to delete | [N]o to cancel deletion.\n ")
             if answer.upper() == "Y":
                 categories.remove(category_to_delete)
-                print("You have successfully deleted the '" + category_to_delete+"' category.")
+                print("You have successfully deleted the '" + category_to_delete + "' category.")
                 write_to_file(categories, "categories.txt", mode="w")
                 break
             elif answer.upper() == "N":
@@ -94,7 +100,7 @@ def delete_category():
                 print("Invalid input entered.")
 
     else:
-        print("There is no such category.")
+        print(f"There is no category '{category_to_delete}'.")
 
 
 def restock():
@@ -114,48 +120,38 @@ def sale():
 
 
 def read_file(file_name):
-    """Reads in a file and stores each line as an element in a list
-
-    [IMPLEMENT ME]
-        1. Open the given file
-        2. Read the file line by line and append each line to a list
-        3. Return the list
-
-    Args:
-        file_name: the name of the file to be read
-
-    Returns:
-        list: a list where each element is a line in the file.
     """
-
-    with open(file_name) as f:
-        content_list = f.readlines()
-        return content_list
+    Read a file or create new if it doesn't exist.
+    :param file_name:
+    :return: File_object or content of file
+    """
+    try:
+        with open(file_name) as f:
+            content_list = f.readlines()
+            return content_list
+    except Exception as e:
+        logging.critical(f"{e}")
+        file_content = open(file_name, mode='r')  # create file if non-existent
+        return file_content
 
 
 def write_to_file(file_contents, output_filename, mode):
-    """Writes the first line of a string to a file.
-
-    [IMPLEMENT]
-        1. Get the first line of file_contents
-        2. Use the File write() function to write the first line into a file
-           with the name from output_filename
-
-        We determine the first line to be everything in a string before the
-        first newline ('\n') character.
-
-    Args:
-        file_contents: string to be split and written into output file
-        output_filename: the name of the file to be written to
+    """
+    Write a file or create new if it doesn't exist.
+    :param file_contents: Content to write to the file
+    :param output_filename:name of file
+    :param mode:mode for opening the file
+    returns: None
     """
 
-    content = file_contents
-    f = open(output_filename, mode)
-    if mode == "a":
-        f.write(content)
-    else:  # modes = "w"
-        print(content)
-        f.writelines(content)
+    try:
+        f = open(output_filename, mode)
+    except Exception as e:
+        logging.critical(f"{e}")
+        f = open(output_filename, mode)  # create file if non-existent
+
+    f.writelines(file_contents)
+    f.close()
 
 
 list_categories()
